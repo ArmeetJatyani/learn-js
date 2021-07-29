e31cd43e77bf7f98462bc42704bc973209c4b27e
ArmeetJatyani <armeetjatyani@gmail.com>
2021-07-29 02:59:44 -0700

    styling (looks fire now)

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 console.log("index.js started");
 
+
 inputField = document.getElementById("input");
 title = document.getElementById("title");
 
+inputField.focus();
 
 function updateTitle() {
     title.innerHTML = inputField.value;
+    inputField.style.width = inputField.value.length + 4 + "ch";
 }
